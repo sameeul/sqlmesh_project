b238d378e967d60a02b2398bbf7008ae47ed912e
Sameeul B Samee <sameeul@gmail.com>
2026-01-05 16:05:06 -0500

    Fix: Correct column names in step02 models

--- models/step02/obs_clin.py
+++ models/step02/obs_clin.py
@@ -1,5 +1,5 @@
 from sqlmesh import ExecutionContext, model
-from util.pcornet.local_schemas import complete_domain_schema_dict, required_domain_schema_dict
+from util.pcornet.step02_columns import complete_domain_schema_dict, required_domain_schema_dict
 from util.pcornet.step02_utils import apply_schema_versioned, validate_required_columns_any, validate_primary_key
 
 
@@ -11,15 +11,16 @@ from util.pcornet.step02_utils import apply_schema_versioned, validate_required_
     columns={"obsclinid": "string", "patid": "string", "encounterid": "string", "obsclin_providerid": "string", "obsclin_start_date": "date", "obsclin_start_time": "string", "obsclin_stop_date": "date", "obsclin_stop_time": "string", "obsclin_type": "string", "obsclin_code": "string", "obsclin_result_qual": "string", "obsclin_result_text": "string", "obsclin_result_snomed": "string", "obsclin_result_num": "double", "obsclin_result_modifier": "string", "obsclin_result_unit": "string", "obsclin_source": "string", "obsclin_abn_ind": "string", "raw_obsclin_name": "string", "raw_obsclin_code": "string", "raw_obsclin_type": "string", "raw_obsclin_result": "string", "raw_obsclin_modifier": "string", "raw_obsclin_unit": "string", "obsclin_date": "date", "obsclin_time": "string"},
 )
 def entrypoint(context: ExecutionContext, **kwargs):
-    df = context.table("pcornet.step01_parsed_obs_clin")
-    required_cols = list(required_domain_schema_dict["obs_clin"].keys())
-    required_cols_v = list(required_domain_schema_dict["obs_clin_5.0"].keys())
+    table_name = context.table("pcornet.step01_parsed_obs_clin")
+    df = context.spark.table(table_name)
+    required_cols = required_domain_schema_dict["obs_clin"]
+    required_cols_v = required_domain_schema_dict["obs_clin_5.0"]
     validate_required_columns_any(df, [required_cols, required_cols_v], "obs_clin")
     df = apply_schema_versioned(
         df,
         complete_domain_schema_dict["obs_clin"],
         complete_domain_schema_dict["obs_clin_5.0"],
-        "obsclin_date",
+        "obsclin_start_date",
     )
     validate_primary_key(df, "obsclinid", "obs_clin")
     return df
